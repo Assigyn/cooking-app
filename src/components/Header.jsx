9141d0e5598575b1fa105cd6db4c712267b7e53a
Anne-sophie <anne.sophie.lesaint@gmail.com>
2025-08-03 10:19:24 +0200

    Feat (assets): Design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import style from './../assets/style/components/header.module.styl'
+import logo from "./../assets/img/logo-dark.svg"
+
 function Header() {
   return (
       <header className={style.Header}>
+          <img src={logo} alt="logo" width="90px"/>
           <h1 className="font-playfair text-x-large m-0">
               Chefs
               <br/><span className="font-playfair text-x-large m-0">Academy</span>
